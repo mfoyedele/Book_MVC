8713f5a9ae0b4750bd1c4b495f8496dab4b6846a
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-08-04 11:32:18 +0100

    Update category model

--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace BookStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public int DisplayOrder { get; set;}
     }
 }

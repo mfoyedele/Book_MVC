4db32f2d35e1908c9ddd55f64310582c32b3d3be
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2023-08-12 04:51:42 +0100

    Orderby method added

--- BookStore/Data/ApplicationDbContext.cs
+++ BookStore/Data/ApplicationDbContext.cs
@@ -16,7 +16,7 @@ namespace BookStore.Data
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
-                new Category { Id = 3, Name = "Action", DisplayOrder = 3 }
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
                 );
         }
     }
---
+++ BookStore/Migrations/20230812034356_SeedCategoryTableTwo.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTableTwo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "History");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Action");
+        }
+    }
+}
